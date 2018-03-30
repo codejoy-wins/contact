8b5e78a2624cb15795bc151f15d4aecb3e1f21de
Max <mjann@ucsc.edu>
2018-03-29 18:13:50 -0700

    drastic changes coming

--- codejoy.js
+++ codejoy.js
@@ -1,4 +1,10 @@
 
+function attachHandlers(){
+    $('.front').click(function(){
+        $(this).hide()
+        $('.back').show()
+    })
+}
 
 $(document).ready(function(){
 
@@ -6,8 +12,12 @@ $(document).ready(function(){
         var firstname = $('input[name=firstname]').val()
         var lastname = $('input[name=lastname]').val()
         var description = $('#ta').val()
-        console.log(lastname)
-        console.log(description)
+        $('#sect-2').append("<div class = 'front'>"+firstname+" "+lastname+"</div>")
+        $('#sect-2').append("<div class = 'back'>"+description+"</div>")
+        $('.back').hide();
+
+        
+        attachHandlers()
         return false;
     })
 
