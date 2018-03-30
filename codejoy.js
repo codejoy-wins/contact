c4bab980f2852532086dbb1e746999807c02155c
Max <mjann@ucsc.edu>
2018-03-29 17:03:20 -0700

    working

--- codejoy.js
+++ codejoy.js
@@ -1,6 +1,19 @@
 
 
 $(document).ready(function(){
+
+    $('form').submit(function(){
+        var firstname = $('input[name=firstname]').val()
+        var lastname = $('input[name=lastname]').val()
+        var description = $('#ta').val()
+        console.log(lastname)
+        console.log(description)
+        return false;
+    })
+
+
+
+
     $('#bt').click(function(){
         alert('Codejoy')
     })
